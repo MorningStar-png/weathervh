db094d78a7507c720cd03f44f5bec10207a54351
Ilia <workplaceilfed@gmail.com>
2020-07-11 17:28:11 +0300

    1 test added

--- tests/test_search.py
+++ tests/test_search.py
@@ -8,3 +8,7 @@ def test_search():
 def test_search():
     w = forecastvh.get_weather('Londond')
     assert w.status == '404'
+
+def test_search():
+    w = forecastvh.get_weather('London', units='metri', lang='e') #Reactions to incorrect variables should not be
+    assert w.status == '200'
